021cb22b16904453b9f175d94f5b71e4939b1978
Andy Casey <andycasey@gmail.com>
2025-01-02 15:11:46 -0500

    dont use lsqr calc_var

--- src/frizzle/__init__.py
+++ src/frizzle/__init__.py
@@ -4,16 +4,12 @@ import numpy.typing as npt
 import warnings
 from pylops import LinearOperator, MatrixMult, Diagonal, Identity
 from scipy.linalg import lstsq
-from scipy.sparse.linalg import lsqr, lsmr, gmres
+from scipy.sparse.linalg import lsqr
 from sklearn.neighbors import KDTree
 from typing import Optional, Union, Tuple
 
-
 from .operator import FrizzleOperator, Diagonal
-from .utils import ensure_dict, check_inputs, separate_flags
-
-
-from .diag_utils import stochastic_lanczos_diag, unit_vector_diagonal_estimator, hutchinson_diagonal_estimator
+from .utils import ensure_dict, check_inputs, separate_flags, combine_flags
 
 def frizzle(
     λ_out: npt.ArrayLike,
@@ -23,15 +19,16 @@ def frizzle(
     mask: Optional[npt.ArrayLike] = None,
     flags: Optional[npt.ArrayLike] = None,
     n_modes: Optional[int] = None,
+    n_uncertainty_samples: Optional[Union[int, float]] = None,
     censor_missing_regions: Optional[bool] = True,
     lsqr_kwds: Optional[dict] = None,
     finufft_kwds: Optional[float] = None,
 ):
     """
-    Combine spectra by forward modelling.
+    Combine spectra by forward modeling.
 
     :param λ_out:
-        The wavelengths to sample the combined spectrum on. This should be shape (M_star, ).
+        The wavelengths to sample the combined spectrum on.
     
     :param λ:
         The wavelengths of the individual spectra. This should be shape (N, ) where N is the number of pixels.
@@ -50,95 +47,79 @@ def frizzle(
         An optional integer array of bitwise flags. If given, this should be shape (N, ).
     
     :param n_modes: [optional]
-        The number of Fourier modes to use. If `None` is given then this will default to `M_star`.
+        The number of Fourier modes to use. If `None` is given then this will default to `len(λ_out)`.
     
-    :param censor_missing_regions: [optional]
-        If `True`, then regions where there is no data will be set to NaN in the resampled spectrum. If `False` the values evaluated
-        from the model will be reported (and have correspondingly large uncertainties) but this will produce unphysical features.
+    :param n_uncertainty_samples: [optional]
+        The number of samples to use when estimating the uncertainty of the combined spectrum by Hutchinson's method. 
+        If `None` is given then this will default to `0.10 * n_modes`. If a float is given then this will be interpreted as a fraction 
+        of `n_modes`. If a number is given that exceeds `n_modes`, the uncertainty will be computed exactly (slow).
 
+    :param censor_missing_regions: [optional]
+        If `True`, then regions where there is no data will be set to NaN in the combined spectrum. If `False` the values evaluated
+        from the model will be reported (and have correspondingly large uncertainties) but this will produce unphysical features.
+        
     :param finufft_kwds: [optional]
         Keyword arguments to pass to the `finufft.Plan()` constructor.
     
     :param lsqr_kwds: [optional]
         Keyword arguments to pass to the `scipy.sparse.linalg.lsqr()` function.
+
+        The most relevant `lsqr()` keyword to the user is `calc_var`, which will compute the variance of the combined spectrum.
+        By default this is set to `True`, but it can be set to `False` to speed up the computation if the variance is not needed.
     
     :returns:
-        A four-length tuple of ``(flux, ivar, flags, meta)`` where ``flux`` is the resampled flux values 
-    and ``ivar`` is the variance of the resampled fluxes, and ``flags`` are the resampled flags, and ``meta`` is a dictionary.
+        A four-length tuple of ``(flux, ivar, flags, meta)`` where:
+            - ``flux`` is the combined fluxes,
+            - ``ivar`` is the variance of the combined fluxes,
+            - ``flags`` are the combined flags, and 
+            - ``meta`` is a dictionary.
     """
 
     n_modes = n_modes or len(λ_out)
-    lsqr_kwds = ensure_dict(lsqr_kwds, calc_var=True)
+    lsqr_kwds = ensure_dict(lsqr_kwds)
     finufft_kwds = ensure_dict(finufft_kwds)
 
-    λ, flux, ivar, mask = check_inputs(λ_out, λ, flux, ivar, mask)
+    λ_out, λ, flux, ivar, mask = check_inputs(λ_out, λ, flux, ivar, mask)
 
     λ_all = np.hstack([λ[~mask], λ_out])
     λ_min, λ_max = (np.min(λ_all), np.max(λ_all))
 
-    # This is setting the scale to be such that the Fourier modes are in the range [0, 2π].
-    scale = 2 * np.pi / (λ_max - λ_min)
+    # This is setting the scale to be such that the Fourier modes are in the range [0, 2π).
+    small = 1e-5
+    scale = (1 - small) * 2 * np.pi / (λ_max - λ_min)
     x = (λ[~mask] - λ_min) * scale
     x_star = (λ_out - λ_min) * scale
+    
+    C_inv_sqrt = np.sqrt(ivar[~mask])
 
     A = FrizzleOperator(x, n_modes, **finufft_kwds)
-    Y = flux[~mask]
     C_inv = Diagonal(np.ascontiguousarray(ivar[~mask]))
-    ATCinv = A.T @ C_inv
 
-    θ, *extras = lsqr(ATCinv @ A, ATCinv @ Y, **lsqr_kwds)
+    A_w = Diagonal(C_inv_sqrt) @ A
+    Y_w = C_inv_sqrt * flux[~mask]
+    θ, *extras = lsqr(A_w, Y_w, **lsqr_kwds)
+    
     meta = dict(zip(["istop", "itn", "r1norm", "r2norm", "anorm", "acond", "arnorm", "xnorm", "var"], extras))
     
+
     A_star = FrizzleOperator(x_star, n_modes, **finufft_kwds)
     y_star = A_star @ θ
 
-    # This is the cheapest way to compute the inverse variance of the resampled spectrum.
-    if lsqr_kwds.get("calc_var", False):
-        C_inv_star = 1/np.diag((A_star @ Diagonal(meta["var"]**0.5) @ A_star.T).todense())
+    # Until I know what lsqr `var` is doing..
+    ATCinvA_inv = lsqr(A.T @ C_inv @ A, np.ones(n_modes), **lsqr_kwds)[0]
+    Op = (A_star @ Diagonal(ATCinvA_inv) @ A_star.T)
 
-        Op = (A_star @ Diagonal(meta["var"]**0.5) @ A_star.T)
-
-
-        from time import time
-        t_dense = -time()
-        foo = np.diag(Op.todense())
-        t_dense += time()
-
-        t_hutchinson = -time()
-        hutchinson = hutchinson_diagonal_estimator(Op.matvec, Op.shape, num_samples=int(0.1 * Op.shape[0]), rademacher=False)
-        t_hutchinson += time()
-
-        print(f"Dense: {t_dense:.2f}s")
-        print(f"Hutchinson: {t_hutchinson:.2f}s (error: {np.linalg.norm(foo - hutchinson)})")
-
-        raise a
-        """
-
-        t_lanczos = -time()
-        bar = stochastic_lanczos_diag(Op.matvec, Op.rmatvec, Op.shape, num_samples=100, lanczos_steps=30)
-        t_lanczos += time()
-
-        t_exact = -time()
-        exact = unit_vector_diagonal_estimator(Op.matvec, Op.shape, num_samples=100)
-        t_exact += time()
-
-        t_hutchinson = -time()
-        hutchinson = hutchinson_diagonal_estimator(Op.matvec, Op.shape, num_samples=30)
-        t_hutchinson += time()
-
-        print(f"Dense: {t_dense:.2f}s")
-        print(f"Lanczos: {t_lanczos:.2f}s (error: {np.linalg.norm(foo - bar)})")
-        print(f"Exact: {t_exact:.2f}s (error: {np.linalg.norm(foo - exact)})")
-        print(f"Hutchinson: {t_hutchinson:.2f}s (error: {np.linalg.norm(foo - hutchinson)})")
-        """
-
-
-    else:
-        C_inv_star = np.zeros_like(y_star)
-
-    # Alternatively:
-    #ATCinvA_inv, *_ = lstsq((A.T @ C_inv @ A).todense(), np.eye(n_modes))
-    #C_star = np.diag((A_star @ MatrixMult(ATCinvA_inv) @ A_star.T).todense())
+    n_uncertainty_samples = n_uncertainty_samples or 0.1
+    if n_uncertainty_samples < 1:
+        n_uncertainty_samples *= n_modes
+        
+    n_uncertainty_samples = int(n_uncertainty_samples)        
+    if n_uncertainty_samples < n_modes:
+        # Estimate the diagonals with Hutchinson's method.
+        v = np.random.randn(n_modes, n_uncertainty_samples)
+        C_inv_star = n_uncertainty_samples/np.sum((Op @ v) * v, axis=1)
+    else:            
+        C_inv_star = 1/np.diag(Op.todense())
     
     if censor_missing_regions:
         # Set NaNs for regions where there were NO data.
@@ -155,15 +136,3 @@ def frizzle(
     flags_star = combine_flags(λ_out, λ, flags)
 
     return (y_star, C_inv_star, flags_star, meta)
-
-
-def combine_flags(x_star, x, flags):
-    flags_star = np.zeros(x_star.size, dtype=np.uint64 if flags is None else flags.dtype)
-    x_star_T = x_star.reshape((-1, 1))
-    diff_x_star = np.diff(x_star)
-    for bit, flag in separate_flags(flags).items():
-        tree = KDTree(x[flag].reshape((-1, 1)))            
-        distances, indices = tree.query(x_star_T, k=1)
-        within_pixel = np.hstack([distances[:-1, 0] <= diff_x_star, False])
-        flags_star[within_pixel] += 2**bit
-    return flags_star
--- src/frizzle/utils.py
+++ src/frizzle/utils.py
@@ -1,7 +1,7 @@
 import numpy as np
 import numpy.typing as npt
 from typing import Optional
-
+from sklearn.neighbors import KDTree
 
 def ensure_dict(d, **defaults):
     kwds = dict()
@@ -16,6 +16,7 @@ def check_inputs(λ_out, λ, flux, ivar, mask):
     else:
         mask = np.hstack(mask).astype(bool)
     
+    λ_out = np.array(λ_out)
     # Mask things outside of the resampling range
     mask *= ((λ_out[0] <= λ) * (λ <= λ_out[-1]))
 
@@ -23,10 +24,8 @@ def check_inputs(λ_out, λ, flux, ivar, mask):
         ivar = np.ones_like(flux)
     else:
         ivar = np.hstack(ivar)    
-    return (λ, flux, ivar, mask)
+    return (λ_out, λ, flux, ivar, mask)
 
-
-    
 def separate_flags(flags: Optional[npt.ArrayLike] = None):
     """
     Separate flags into a dictionary of flags for each bit.
@@ -45,3 +44,25 @@ def separate_flags(flags: Optional[npt.ArrayLike] = None):
                 separated[q] = is_set.astype(bool)
     return separated    
 
+def combine_flags(λ_out, λ, flags):
+    """
+    Combine flags from input spectra.
+
+    :param λ_out:
+        The wavelengths to sample the combined spectrum on.
+    
+    :param λ:
+        The input wavelengths.
+    
+    :param flags:
+        An array of integer flags.
+    """
+    flags_star = np.zeros(λ_out.size, dtype=np.uint64 if flags is None else flags.dtype)
+    λ_out_T = λ_out.reshape((-1, 1))
+    diff_λ_out = np.diff(λ_out)
+    for bit, flag in separate_flags(flags).items():
+        tree = KDTree(λ[flag].reshape((-1, 1)))            
+        distances, indices = tree.query(λ_out_T, k=1)
+        within_pixel = np.hstack([distances[:-1, 0] <= diff_λ_out, False])
+        flags_star[within_pixel] += 2**bit
+    return flags_star
